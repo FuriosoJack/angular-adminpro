b15a5d333b17bdaba9dd9803b177df67b5da4752
Juan Carlos <disenoweb@tae-ltda.com>
2019-10-07 08:36:41 -0500

    Se actualiza repo

---
+++ src/app/shared/notpagefount/notpagefount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagefount',
+  templateUrl: './notpagefount.component.html',
+  styles: []
+})
+export class NotpagefountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
